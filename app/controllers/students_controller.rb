29f7d0547554d00dbf5a3e95c3a6c2131b3ae9b2
Rani Shah <rani.shah@flatironschool.com>
2018-04-09 11:36:24 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,10 +1,24 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    redirect_to students_path
+  end
+
+  private
+
+  def set_student
     @student = Student.find(params[:id])
   end
 
